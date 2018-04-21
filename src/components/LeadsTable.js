88c6ff99e76aff1ea0b1c01df79632f55e1a6620
Zach Gilbert <zsgilber@gmail.com>
2018-04-20 17:45:24 -0700

    added selectable column from ant design example

--- src/components/LeadsTable.js
+++ src/components/LeadsTable.js
@@ -1,10 +1,55 @@
 import React from "react";
-import { Table } from 'antd';
+import { Table, Button } from 'antd';
 
-function LeadsTable(props) {
-  return (
-      <Table rowKey= "id" dataSource={props.dataSource} columns={props.columns} />
-  );
+class LeadsTable extends React.Component {
+  state = {
+    selectedRowKeys: [], // Check here to configure the default column
+    loading: false,
+  };
+  start = () => {
+    this.setState({ loading: true });
+    // ajax request after empty completing
+    setTimeout(() => {
+      console.log(this.state.selectedRowKeys);
+      this.setState({
+        selectedRowKeys: [],
+        loading: false,
+      });
+    }, 1000);
+  }
+  onSelectChange = (selectedRowKeys) => {
+    console.log('selectedRowKeys changed: ', selectedRowKeys);
+    this.setState({ selectedRowKeys });
+  }
+  render() {
+    const { loading, selectedRowKeys } = this.state;
+    const rowSelection = {
+      selectedRowKeys,
+      onChange: this.onSelectChange,
+    };
+    const hasSelected = selectedRowKeys.length > 0;
+    return (
+      <div>
+        <div style={{ marginBottom: 16 }}>
+          <Button
+            type="primary"
+            onClick={this.start}
+            disabled={!hasSelected}
+            loading={loading}
+          >
+            Reload
+          </Button>
+          <span style={{ marginLeft: 8 }}>
+            {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
+          </span>
+        </div>
+        <Table rowKey="id" rowSelection={rowSelection} columns={this.props.columns} dataSource={this.props.dataSource} />
+      </div>
+    );
+  }
 }
+
+      //<Table rowKey= "id" dataSource={this.props.dataSource} columns={this.props.columns} />
+
 
 export default LeadsTable;

14e1ba558c256c776ba7928253d7441c28f9897d
Zach Gilbert <zsgilber@gmail.com>
2018-05-05 14:19:42 -0700

    API only needs to return data, not manage state

--- src/api/leadApi.js
+++ src/api/leadApi.js
@@ -1,23 +1,16 @@
+import axios from "axios";
+
 class leadApi {
 
   static getLeads() {
-      return
-      axios
+      return axios
       .get("/api/leads")
       .then(response => {
-        const newLeads = response.data;
-
-        // create a new "State" object without mutating
-        // the original State object.
-        const newState = Object.assign({}, this.state, {
-          leads: newLeads
-        });
-
-        // store the new state object in the component's state
-        console.log(newState);
-        this.setState(newState);
+        const leads = response.data;
+        return leads;
       })
       .catch(error => console.log(error));
   }
+};
 
 export default leadApi;

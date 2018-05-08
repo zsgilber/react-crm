b93345c8475e0659f8bb03216b1cbdc773273ba9
Zach Gilbert <zsgilber@gmail.com>
2018-05-07 23:22:52 -0700

    simple bar chart

--- src/components/DashboardContainer.js
+++ src/components/DashboardContainer.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
+import { OrdinalFrame } from "semiotic";
+import { Layout, Menu, Breadcrumb, Icon, Row, Col } from "antd";
 
+const { Header, Content, Footer, Sider } = Layout;
+
+const barChartData = [
+  { category: "Leads", sum: 10}
+];
 
 class DashboardContainer extends Component {
-
   render() {
     return (
-      <div><h1>Dashboard</h1></div>
-    )
+      <Layout>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content>
+          <Row>
+            <Col span={6}>
+              {" "}
+              <OrdinalFrame
+                size={[100, 300]}
+                data={barChartData}
+                oAccessor={"category"}
+                rAccessor={"sum"}
+                style={{ fill: "#00a2ce", stroke: "white" }}
+                type={"bar"}
+                oLabel={true}
+              />
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
+    );
   }
 }
 
